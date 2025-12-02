0ab4e9800403676c998c579d60d5e04aef11a850
che-devs <cehrensberger@technorama.ch>
2025-12-02 16:52:20 +0100

    add venv commands and echos

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cd /home/pi/mass-infection/
+
+echo "Navigating to the project directory..."
+cd ~/mass-infection/
+
+echo "Activating the virtual environment..."
+source .venv/bin/activate
+
+echo "Starting the main Python application..."
 python3 main.py
